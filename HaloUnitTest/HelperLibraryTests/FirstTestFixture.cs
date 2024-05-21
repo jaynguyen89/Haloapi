f77c3f394e2c62955a8ee02fa5c387ac2747eb6a
Jay Nguyen <nguyen.le.kim.phuc@gmail.com>
2024-05-21 21:00:11 +1000

    Remove dB mocks

--- HaloUnitTest/HelperLibraryTests/FirstTestFixture.cs
+++ HaloUnitTest/HelperLibraryTests/FirstTestFixture.cs
@@ -1,16 +1,9 @@
-using HaloUnitTest.Mocks;
-using Microsoft.EntityFrameworkCore;
-
 namespace HaloUnitTest.HelperLibraryTests;
 
 public class FirstTestFixture {
     
     [Test]
     public async Task TestIfAnything() {
-        //var dbContextMock = HalogenDbContextMock.Instance();
-        //await dbContextMock.WithData(new Dictionary<string, List<object>>());
-        var dbContext = await HalogenDbContextMock.Instance().MemoryDbContext();
-
-        var some = await dbContext.Accounts.Take(1).Include(a => a.Roles).SelectMany(a => a.Roles.ToArray()).ToArrayAsync();
+        
     }
 }
